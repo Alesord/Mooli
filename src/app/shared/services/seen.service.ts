03d7a5b3d52934d4412c7a8f908d218724940f99
Alesord <alesord@gmail.com>
2022-07-28 20:03:17 -0300

    Cambios de la pagina de todas las peliculas-checks

---
+++ src/app/shared/services/seen.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Movie } from '../models/imdbMovies.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeenService {
+  private baseUrl = 'https://mooli-3d0bf-default-rtdb.firebaseio.com/seenList/-N85rm2-iiM4AsN7ObzU/'
+  private movies: Movie[]
+
+  constructor(private http: HttpClient) { }
+
+  OnSendRequest(id: string, seen: boolean) {
+    this.http.put(this.baseUrl + id + '.json', {seen}).subscribe({next: (respuestaData => {
+        console.log('Se ha posteado ' + respuestaData);
+      })
+    })
+  }
+
+  
+  OnGetSeen(id: string) {
+    return this.http.get(this.baseUrl + id + '.json')
+  }
+}
