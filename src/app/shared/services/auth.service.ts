5e52ca7cfdf9acd48353f51aa6ae80ed6d5eb522
Alesord <alesord@gmail.com>
2022-08-19 15:36:29 -0300

    User Implementacion

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,12 @@ export class AuthService {
   url: string = environment.URL_FLAT;
   private _userAutenticado: boolean = false;
   private _userKey: string;
+
+  usersX: User = {
+    login: 'user@user.com',
+    password: 'user',
+    token: '-N8PnJ6s8FDh77vUleJp'
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -25,11 +32,35 @@ export class AuthService {
     this._userKey = userKey;
   }
 
+  register(username: string, password: string) {
+    let newUser: User = {
+      login: username,
+      password: password,
+      token: this.crearToken()
+    }
+    console.log(newUser.token)
+    console.log(newUser)
+    this.http.put(`${this.url}/usersBD/${newUser.token}.json`, newUser).subscribe()
+    
+    this.http.put(`${this.url}/usersBD/${newUser.token}.json`, newUser).subscribe()
+  }
+
   logout() {
     this._userAutenticado = false;
   }
 
-  getUserey() {
-    this.http.post(`${this.url}/usersBD.json`,{nombre: 'a'}).subscribe()
+  checkLogin() {
+    return this.http.get(`${this.url}/usersBD.json`)
+  }
+
+  crearToken() {
+    var result = '-';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = 19;
+    for (var i = 0; i < 19; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
   }
 }
