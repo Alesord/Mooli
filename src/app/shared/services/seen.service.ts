c2408c95df61c7bf250eae508fb0d0b216639907
Alesord <alesord@gmail.com>
2022-08-26 12:25:14 -0300

    Logout storage clear

--- src/app/shared/services/seen.service.ts
+++ src/app/shared/services/seen.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeenService {
-  private baseUrl = environment.URL_BD_SEEN
-
-  constructor(private http: HttpClient) { }
-
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+    ) { }
+  // URL_BD_SEEN: `${URL_MOOLI}/users/-N8PnJ6s8FDh77vUleJp/seenList/`,
+  private userId: string = this.authService.userKey;
+  private baseUrl = `${environment.URL_USERS}/${this.userId}/seenList/`
+  
   OnSendRequest(id: string, seen: boolean) {
     return this.http.put(this.baseUrl + id + '.json', {seen})
   }
